7ab4d28efd30fc0b8be1f253e141440ebffa9c13
danajamilla <94362031+danajamilla@users.noreply.github.com>
2021-12-13 15:24:41 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-// JavaScript Document
+var hamburgerMenu = document.querySelector("nav button:nth-of-type(3)");
+
+hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
+hamburgerMenu.addEventListener("click")
+function toggleHamburgerMenu() {
+    var navElement = document.querySelector("nav");
+    navElement.classList.toggle("menuOpen");
+}
+
+
+
