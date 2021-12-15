cdda57c5c08ab93db719b14726f1b2c3ed4e1eb2
danajamilla <94362031+danajamilla@users.noreply.github.com>
2021-12-15 14:20:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var hamburgerMenu = document.querySelector("nav button:nth-of-type(3)");
-hamburgerMenu.addEventListener("click", toggleHamburgerMenu);
-
-function toggleHamburgerMenu() {
-    var navElement = document.querySelector("nav");
-    navElement.classList.toggle("menuOpen");
-}
